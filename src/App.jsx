45a77a3bc74b5171254946176f88d39aae26f815
MarlaRey <hejmarlenehej@gmail.com>
2024-04-25 11:31:12 +0200

    host

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import {Frontpage} from "./pages/Frontpage"
 import Aboutpage from './pages/Aboutpage'
 import {Layout} from './Layout/Layout'
